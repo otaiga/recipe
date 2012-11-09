ac6c574d585cecdbded91723d70dc6176c86bc91
richlewis14 <richlewis14@gmail.com>
2012-11-09 22:45:11 +0000

    search mutliple columns

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -6,7 +6,16 @@ class SearchController < ApplicationController
 
   def search
 
-     @countrysearch = Recipe.where(:country_of_origin => params[:search]).all
+  #@countrysearch = Recipe.where(:country_of_origin => params[:search]).all
+  #@countrysearch = Recipe.where(:dish_name => params[:search], :country_of_origin => params[:search], :difficulty => params[:search], :preperation_time => params[:search]).all
+  #@countrysearch = Recipe.where(params[:search]).all
+  #@countrysearch = Recipe.where(:dish_name => params[:search]).all
+  @countrysearch = Recipe.where("dish_name = ? OR country_of_origin = ? OR difficulty = ? OR preperation_time = ?",
+    params[:search],
+    params[:search],
+    params[:search],
+    params[:search]
+)
   end
 
 end
