2370e0505089f7abba4970c39d611ec8aa37557e
richlewis14 <richlewis14@gmail.com>
2012-12-08 12:26:05 +0000

    added css to myrecipes

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+
+  def display_login
+    if !current_user
+      link_to "Log In", new_user_session_path
+    else
+      link_to "Logout", destroy_user_session_path, :method => :delete
+    end
+  end
 end
