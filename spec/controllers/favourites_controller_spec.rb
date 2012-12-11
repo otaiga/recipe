365355f175310b351d550326ba5d08ce795f8fd7
otaiga <karl.turner5@gmail.com>
2012-12-11 08:09:15 +0000

    added new test:

--- spec/controllers/favourites_controller_spec.rb
+++ spec/controllers/favourites_controller_spec.rb
@@ -4,12 +4,22 @@ describe FavouritesController do
   before(:each) do
     @new_user =  User.create!(name: "test_user2", email: "test_user2@example.com", password: "password")
     @new_user.confirm!
-    sign_in @new_user
   end
 
   it "should create a favorite for a signed in user" do
+    sign_in @new_user
     post :create, recipe_id: given_recipe(given_user).id
     @new_user.favourites.count.should == 1
+    @new_user.favourites.first.recipe.should == given_recipe(given_user)
+  end
+
+  it "should be able to add own recipes to favourites" do
+    given_user.confirm!
+    sign_in given_user
+    post :create, recipe_id: given_recipe(given_user).id
+    given_user.reload
+    given_user.favourites.count.should == 1
+    given_user.favourites.first.recipe.should == given_recipe(given_user)
   end
 
 end
